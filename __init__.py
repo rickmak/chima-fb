3b584cfe0e639b3ba3942bc1060aca1b237f0bbf
Rick Mak <rick.mak@gmail.com>
2016-04-14 15:41:57 +0800

    Add facebook verification

--- __init__.py
+++ __init__.py
@@ -2,4 +2,7 @@ import skygear
 
 @skygear.handler('chima')
 def chima(request):
-    return 'fasengisfat'
+    if request.values.get('hub.verify_token') == 'fasengisfat':
+        return request.values.get('hub.challenge')
+    else:
+        return 'You are not facebook'
