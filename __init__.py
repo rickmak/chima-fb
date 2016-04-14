53db950498398d324f33c77387db98b093e29bdc
Rick Mak <rick.mak@gmail.com>
2016-04-14 16:12:56 +0800

    Add echo of message

--- __init__.py
+++ __init__.py
@@ -1,8 +1,50 @@
+import os
+import logging
+import json
+
+import requests
 import skygear
 
+FB_VERIFY = os.getenv('FB_VERIFY')
+CHIMA_TOKEN = os.getenv('CHIMA_TOKEN')
+
+log = logging.getLogger(__name__)
+
+
 @skygear.handler('chima')
-def chima(request):
-    if request.values.get('hub.verify_token') == 'fasengisfat':
+def verify(request, method=['GET']):
+    if request.values.get('hub.verify_token') == FB_VERIFY:
         return request.values.get('hub.challenge')
     else:
         return 'You are not facebook'
+
+
+@skygear.handler('chima')
+def echo(request, method=['POST']):
+    log.info('Got message from facebook')
+    body = request.get_data(as_text=True)
+    payload = json.loads(body)
+    log.info(payload)
+    events = payload['entry'][0]['messaging']
+    for evt in events:
+        sender = evt['sender']['id']
+        if 'message' in evt:
+            msg = evt['message']
+            r = requests.post(
+                'https://graph.facebook.com/v2.6/me/messages',
+                params={
+                    'access_token': CHIMA_TOKEN
+                },
+                json={
+                    'recipient': {
+                        'id': sender
+                    },
+                    'message': {
+                        'text': msg['text']
+                    }
+                }
+            )
+            log.info(r.json())
+        else:
+            log.info('Cannot handle')
+
